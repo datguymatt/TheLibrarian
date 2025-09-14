729d54454290290171d220cc0290b33fae718bf3
datguymatt <128839182+datguymatt@users.noreply.github.com>
2025-09-14 08:51:30 -0700

    started making UI Manager, adjusted music

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    //singleton
+    public static UIManager Instance;
+
+    //text objects
+    [SerializeField] TextMeshProUGUI narrationText;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void UpdateNarrationText(string text)
+    {
+        narrationText.text = text;
+    }
+
+
+}
